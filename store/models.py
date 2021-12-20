d7f69e8527c79ed2f433346d11ee2c0f51224a7f
Chukwuma Obasi <69759467+ccobasi@users.noreply.github.com>
2021-12-20 11:59:43 +0100

    add customer and product model

--- store/models.py
+++ store/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Customer(models.Model):
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, null=True, blank=True)
+    name = models.CharField(max_length=200, null=True)
+    email = models.EmailField(max_length=150, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=200, null=True)
+    price = models.FloatField()
+    digit = models.BooleanField(default=False, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
