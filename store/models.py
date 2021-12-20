8bf8b779248a12983bf077775dba0fffcba47a77
Chukwuma Obasi <69759467+ccobasi@users.noreply.github.com>
2021-12-20 12:24:24 +0100

    add orderitem and shipping address

--- store/models.py
+++ store/models.py
@@ -6,7 +6,7 @@ class Customer(models.Model):
     user = models.OneToOneField(
         User, on_delete=models.CASCADE, null=True, blank=True)
     name = models.CharField(max_length=200, null=True)
-    email = models.EmailField(max_length=150, null=True)
+    email = models.EmailField(max_length=200, null=True)
 
     def __str__(self):
         return self.name
@@ -19,3 +19,40 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Order(models.Model):
+    customer = models.ForeignKey(
+        Customer, on_delete=models.SET_NULL, null=True, blank=True)
+    date_ordered = models.DateTimeField(auto_now_add=True)
+    completed = models.BooleanField(default=False, null=True, blank=True)
+    transaction_id = models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return str(self.id)
+
+
+class OrderItem(models.Model):
+    product = models.ForeignKey(
+        Product, on_delete=models.SET_NULL, null=True, blank=True)
+    order = models.ForeignKey(
+        Order, on_delete=models.SET_NULL, null=True, blank=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+    quantity = models.IntegerField(default=0, null=True, blank=True)
+
+    def __str__(self):
+        return self.product
+
+
+class ShippingAddress(models.Model):
+    customer = models.ForeignKey(
+        Customer, on_delete=models.SET_NULL, null=True)
+    order = models.ForeignKey(
+        Order, on_delete=models.SET_NULL, null=True)
+    address = models.CharField(max_length=250, null=False)
+    city = models.CharField(max_length=50, null=False)
+    state = models.CharField(max_length=50, null=False)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.address
