9c4e6143143aa41cc25c73236c60059579485cb0
Chukwuma Obasi <69759467+ccobasi@users.noreply.github.com>
2021-12-21 21:44:34 +0100

    update

--- store/views.py
+++ store/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+import json
 from .models import *
 
 
@@ -32,3 +34,52 @@ def checkout(request):
         order = {'get_cart_total': 0, 'get_cart_items': 0}
     context = {'items': items, 'order': order}
     return render(request, 'store/checkout.html', context)
+
+
+def updateItem(request):
+    data = json.loads(request.body)
+    productId = data['productId']
+    action = data['action']
+    print('Action:', action)
+    print('productId:', productId)
+
+    customer = request.user.customer
+    product = Product.objects.get(id=productId)
+    order, created = Order.objects.get_or_create(
+        customer=customer, completed=False)
+    orderItem, created = OrderItem.objects.get_or_create(
+        order=order, product=product)
+
+    if action == 'add':
+        orderItem, quantity = (orderItem.quantity + 1)
+    elif action == 'remove':
+        orderItem.quantity = (orderItem.quantity - 1)
+    orderItem.save()
+
+    if orderItem.quantity <= 0:
+        orderItem.delete()
+    # data = json.loads(request.body)
+    # productId = data['productId']
+    # action = data['action']
+    # print('Action:', action)
+    # print('Product:', productId)
+
+    # customer = request.user.customer
+    # product = Product.objects.get(id=productId)
+    # order, created = Order.objects.get_or_create(
+    #     customer=customer, complete=False)
+
+    # orderItem, created = OrderItem.objects.get_or_create(
+    #     order=order, product=product)
+
+    # if action == 'add':
+    #     orderItem.quantity = (orderItem.quantity + 1)
+    # elif action == 'remove':
+    #     orderItem.quantity = (orderItem.quantity - 1)
+
+    # orderItem.save()
+
+    # if orderItem.quantity <= 0:
+    #     orderItem.delete()
+
+    return JsonResponse('Item was added', safe=False)
