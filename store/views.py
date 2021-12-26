01d1dbf009aa4a47ab52a4bb1757ab2455be91fb
Chukwuma Obasi <69759467+ccobasi@users.noreply.github.com>
2021-12-26 10:56:52 +0100

    add cartItems to checkout and cart views

--- store/views.py
+++ store/views.py
@@ -26,10 +26,12 @@ def cart(request):
         order, created = Order.objects.get_or_create(
             customer=customer, completed=False)
         items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
     else:
         items = []
         order = {'get_cart_total': 0, 'get_cart_items': 0}
-    context = {'items': items, 'order': order}
+        cartItems = order['get_cart_items']
+    context = {'items': items, 'order': order, 'cartItems': cartItems}
     return render(request, 'store/cart.html', context)
 
 
@@ -38,11 +40,12 @@ def checkout(request):
         customer = request.user.customer
         order, created = Order.objects.get_or_create(
             customer=customer, completed=False)
-        items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
     else:
         items = []
         order = {'get_cart_total': 0, 'get_cart_items': 0}
-    context = {'items': items, 'order': order}
+        cartItems = order['get_cart_items']
+    context = {'items': items, 'order': order, 'cartItems': cartItems}
     return render(request, 'store/checkout.html', context)
 
 
