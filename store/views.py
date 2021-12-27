919aa189b3743558aae138cfa6ac8957ae438328
Chukwuma Obasi <69759467+ccobasi@users.noreply.github.com>
2021-12-27 14:43:59 +0100

    add guest checkout logic

--- store/views.py
+++ store/views.py
@@ -74,11 +74,42 @@ def processOrder(request):
         customer = request.user.customer
         order, created = Order.objects.get_or_create(
             customer=customer, completed=False)
-        total = float(data['form']['total'])
-        order.transaction_id = transaction_id
-        if total == order.get_cart_total:
-            order.completed = True
-        order.save()
+
+    else:
+        print('User is not logged in')
+
+        print('COOKIES:', request.COOKIES)
+        name = data['form']['name']
+        email = data['form']['email']
+
+        cookieData = cookieCart(request)
+        items = cookieData['items']
+
+        customer, created = Customer.objects.get_or_create(
+            email=email
+        )
+        customer.name = name
+        customer.save()
+
+        order = Order.objects.create(
+            customer=customer,
+            completed=False
+        )
+
+        for item in items:
+            product = Product.objects.get(id=item['product']['id'])
+
+            orderItem = OrderItem.objects.create(
+                product=product,
+                order=order,
+                quantity=item['quantity']
+            )
+
+    total = float(data['form']['total'])
+    order.transaction_id = transaction_id
+    if total == order.get_cart_total:
+        order.completed = True
+    order.save()
 
     if order.shipping == True:
         ShippingAddress.objects.create(
@@ -89,7 +120,5 @@ def processOrder(request):
             state=data['shipping']['state'],
             zipcode=data['shipping']['zipcode'],
         )
-    else:
-        print('User is not logged in')
 
     return JsonResponse('Payment submitted..', safe=False)
